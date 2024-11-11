251cc85a9ffdc9fdd91b96fd8a44ae1c4255d985
DainfromLiria <mayor3217@gmail.com>
2024-11-11 20:57:48 +0100

    minor changes

--- src/configs/config.py
+++ src/configs/config.py
@@ -13,9 +13,12 @@ class DataConfig:
     AUG_DATASET_INFO_PATH: str = "../aug_data/info.json"
 
     AUGMENTATIONS: list = field(default_factory=lambda: [
-        A.HorizontalFlip(p=1.0), A.VerticalFlip(p=1.0),
+        A.HorizontalFlip(p=1.0),
+        A.VerticalFlip(p=1.0),
         A.RandomToneCurve(scale=0.3, p=1.0),  # RandomToneCurve change only brightness and contrast
-        A.GaussNoise(p=1.0), A.PixelDropout(mask_drop_value=0.0, p=1.0), A.Sharpen(p=1.0),
+        A.GaussNoise(p=1.0),
+        A.PixelDropout(mask_drop_value=0.0, p=1.0),
+        A.Sharpen(p=1.0),
         A.GridElasticDeform(num_grid_xy=(10, 10), magnitude=30, p=1.0),
         A.ShiftScaleRotate(p=1.0)  # use it instead of Rotate because it can make rotate, translate or scale
     ])
