5430a9542ab5a9db23b85e08a9861b8ec75ca920
DainfromLiria <mayor3217@gmail.com>
2024-11-14 23:17:16 +0100

    main for tests

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
+from configs.config import config
 from dataset.augmentation import Augmentation
 from dataset.dataset import SandGrainsDataset
+from detector.detector import MicroTextureDetector
+from utils.logging import setup_logging
+import logging
+
+setup_logging()
+
+logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    aug = Augmentation()
-    aug.augment()
-    # dt = SandGrainsDataset()
-    # print(len(dt))
+    # aug = Augmentation()
+    # aug.augment()
+
+    dt = SandGrainsDataset(path=config.data.AUG_TRAIN_SET_PATH)
+    img, masks = dt[0]
+    logger.info(f"Image shape: {img.shape} - Mask shape: {masks.shape}")
+
+    # net = MicroTextureDetector()
+    # net.train()
 
