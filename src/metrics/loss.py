a39bbf1c81e7051052ac3ab9bd1333cb2ccbcfe6
DainfromLiria <mayor3217@gmail.com>
2024-12-01 23:15:33 +0100

    Add FocalTverskyLoss.

--- src/metrics/loss.py
+++ src/metrics/loss.py
@@ -5,31 +5,33 @@ import torch.nn.functional as F
 from torch import nn
 
 from configs.config import config
+from metrics import convert_nn_output
 
 logger = logging.getLogger(__name__)
 
 
+# ====================================================================================================================
 class FocalLoss(nn.Module):
     def __init__(self):
         super(FocalLoss, self).__init__()
 
-    def forward(self, inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+    def forward(self, outputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
         """
         Calculate alpha weighted focal loss for all input batch. Use mean for reduction.
 
-        :param inputs: output of last nn layer.
+        :param outputs: output of last nn layer.
         :param targets: masks.
 
         :return: mean alpha weighted focal loss for batch.
         """
-        inputs = torch.sigmoid(inputs)
+        outputs = convert_nn_output(outputs=outputs, to_mask=False)
 
-        inputs = inputs.flatten()
+        outputs = outputs.flatten()
         targets = targets.flatten()
         alpha = self._get_alpha(targets)
-        bce_loss = F.binary_cross_entropy(weight=alpha, input=inputs, target=targets, reduction='none')  # -log(pt)
+        bce_loss = F.binary_cross_entropy(weight=alpha, input=outputs, target=targets, reduction='none')  # -log(pt)
         pt = torch.exp(-bce_loss)
-        focal_loss_per_pixel = (torch.tensor(1) - pt)**config.model.GAMMA * bce_loss
+        focal_loss_per_pixel = (torch.tensor(1) - pt)**config.model.F_GAMMA * bce_loss
         return torch.mean(focal_loss_per_pixel)
 
     @staticmethod
@@ -53,3 +55,26 @@ class FocalLoss(nn.Module):
         except Exception as e:
             logger.error(f"{e}")  # in case if all mask contains only 0 or 1
             raise e
+# ====================================================================================================================
+
+
+# ====================================================================================================================
+class FocalTverskyLoss(nn.Module):
+    def __init__(self):
+        super(FocalTverskyLoss, self).__init__()
+
+    def forward(self, outputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        eps = 1e-6  # for situations when mask and target contains only zeros
+        outputs = convert_nn_output(outputs=outputs, to_mask=False)
+
+        # ==========================================================================================================
+        # https://github.com/Project-MONAI/MONAI/blob/46a5272196a6c2590ca2589029eed8e4d56ff008/monai/losses/tversky.py
+        tp = torch.sum(outputs * targets, dim=0)
+        fn = torch.sum((1 - outputs) * targets, dim=0)
+        fp = torch.sum(outputs * (1 - targets), dim=0)
+        # ==========================================================================================================
+
+        ti = (tp + eps) / (tp + (config.model.FT_ALPHA * fn) + (config.model.FT_BETA * fp) + eps)
+        tfl = (1 - ti)**config.model.FT_GAMMA
+        return torch.mean(tfl.squeeze())
+# ====================================================================================================================
