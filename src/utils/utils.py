e5908dc9e914a8d0047385f00648e3a78e7295bc
DainfromLiria <mayor3217@gmail.com>
2025-01-08 21:55:24 +0100

    Add docstring.

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,6 +3,15 @@ from configs import config
 
 
 def predict_morphological_feature(outputs: torch.Tensor) -> torch.Tensor:
+    """
+    Select only one type of relief and one type of shape.
+    Keep it and make all other predictions of relief and shape in outputs mask as zero.
+    The best prediction is selected by max count of predicted pixels.
+
+    :param outputs: tensor with predictions.
+
+    :return: tensor with updated predictions where only one relief and one shape type has pixels with 1 value.
+    """
     relief_idx = config.data.RELIEF_IDX
     shape_idx = config.data.SHAPE_IDX
 
