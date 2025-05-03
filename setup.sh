e0fca19808fc0e6559d50fa00a7f759a4ed1fcae
DainfromLiria <mayor3217@gmail.com>
2025-05-03 14:35:50 +0200

    add setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if command -v python3 >/dev/null 2>&1; then
+    echo "Python 3 is installed"
+    python3 --version
+else
+    echo "Python 3 is not installed! Please install it."
+fi
+
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install --no-dependencies git+https://github.com/nasa/pretrained-microscopy-models
